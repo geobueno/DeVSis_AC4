8f5bf6c7db4a0063825719aa0cc02613979b850c
Luciana Silveira <1800005@SALAS.AULAS>
2018-09-21 21:54:59 -0300

    funcao

---
+++ grupos.py
@@ -0,0 +1,10 @@
+alunos=[]
+
+def gravaAlunos():
+   
+    alunos.append({"RA":1, "Nome":"Bla1"})
+    alunos.append({"RA":1, "Nome":"Bla2"})
+    return
+
+def listaAlunos():
+    return alunos
--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
+import grupos
 
 app = Flask(__name__)
 
-@app.route ("/", methods=["GET"])
+
+#templates de direcionamento
 @app.route ("/home", methods=["GET"])
 def home():
     return render_template("index.html")
@@ -19,6 +21,18 @@ def contato():
 @app.route ("/sobre", methods=["GET"])
 def sobre():
     return render_template("sobre.html")
+
+#templates de funcoes
+@app.route ("/", methods=["GET"])
+@app.route ("/home", methods=["GET"])
+def index():
+    grupos.gravaAlunos()
+    nomesAlunos = grupos.listaAlunos()
+    return render_template("index.html", nomes=nomesAlunos)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(port=80)
